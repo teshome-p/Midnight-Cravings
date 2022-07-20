3366d4a64f26c4837c79316136a38eba62c72fe0
Velma Anyona <vanyona@u.rochester.edu>
2022-07-20 19:00:42 +0000

    Initial comit for all files

---
+++ account.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
+from flask_behind_proxy import FlaskBehindProxy
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+proxied = FlaskBehindProxy(app)  ## add this line
+
+app.config['SECRET_KEY'] = '48103c3f1e535f696813b3bcb1321b7b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+
+
+@app.route("/website")
+def home():
+    return render_template('website.html', subtitle='Home Page', text='This is the home page')
+
+@app.route("/home")
+def second_page():
+    return render_template('home.html', subtitle='Home Page', text='This is the home page')
+
+@app.route("/", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
+
+
+if __name__ == '__main__':               # this should always be at the end
+    app.run(debug=True, host="0.0.0.0")
